be57184830f68c586746bc62922af1eb10be0ef6
Alfie John Casama <alfiecasama220@gmail.com>
2024-11-27 05:27:40 +0800

    Database export added

--- public/admin/database/aclmsloan.sql
+++ public/admin/database/aclmsloan.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2024 at 06:37 AM
--- Server version: 10.4.32-MariaDB
+-- Generation Time: Nov 26, 2024 at 10:27 PM
+-- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,23 +28,109 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `borrowers` (
-  `id` int(30) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
   `firstname` varchar(100) NOT NULL,
   `middlename` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
-  `contact_no` varchar(30) NOT NULL,
   `address` text NOT NULL,
+  `sex` varchar(50) NOT NULL,
+  `age` varchar(100) NOT NULL,
+  `civil` varchar(100) NOT NULL,
+  `religion` varchar(100) NOT NULL,
+  `contact_no` varchar(30) NOT NULL,
+  `dateOfBirth` varchar(100) NOT NULL,
+  `placeOfBirth` varchar(100) NOT NULL,
+  `yearLevel` varchar(50) NOT NULL,
+  `course` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `tax_id` varchar(50) NOT NULL,
-  `date_created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `tax_id` varchar(50) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `borrowers`
 --
 
-INSERT INTO `borrowers` (`id`, `firstname`, `middlename`, `lastname`, `contact_no`, `address`, `email`, `tax_id`, `date_created`) VALUES
-(2, 'Sheeee', '', 'Shells', '123', 'Dgte', 'shelbylynchsilot@gmail.com', '', 0);
+INSERT INTO `borrowers` (`id`, `firstname`, `middlename`, `lastname`, `address`, `sex`, `age`, `civil`, `religion`, `contact_no`, `dateOfBirth`, `placeOfBirth`, `yearLevel`, `course`, `email`, `username`, `password`, `tax_id`, `created_at`, `updated_at`) VALUES
+(2, 'Alfie John', 'G.', 'Casama', 'Bunao, Dumaguete City', 'male', '22', 'Single', 'Roman Catholicism', '09922788868', '11/10/2002', 'Dumaguete City', '4th', 'Bachelor of Science in Computer Science', 'alfiecasama220@gmail.com', 'alfiecasama220', '$2y$12$lFa3AhuenISh/OY9TUFsmOp7YzeQ2A5hxkdabZewrSu34VpmyfDje', '0', '2024-11-26 11:58:01', '2024-11-26 11:58:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+
+CREATE TABLE `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+
+CREATE TABLE `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+
+CREATE TABLE `job_batches` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -65,16 +151,6 @@ CREATE TABLE `loan_list` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `loan_list`
---
-
-INSERT INTO `loan_list` (`id`, `ref_no`, `loan_type_id`, `borrower_id`, `purpose`, `amount`, `plan_id`, `status`, `date_released`, `date_created`) VALUES
-(4, '12697579', 1, 2, 'fee', 10000, 4, 0, '0000-00-00 00:00:00', '2024-04-24 08:39:28'),
-(5, '37749521', 1, 1, 'Fee2', 20000, 4, 1, '0000-00-00 00:00:00', '2024-04-23 19:51:19'),
-(6, '80457274', 1, 2, '', 100, 4, 1, '0000-00-00 00:00:00', '2024-04-24 08:19:33'),
-(7, '31390307', 4, 2, '', 10000, 5, 0, '0000-00-00 00:00:00', '2024-04-24 10:57:51');
-
 -- --------------------------------------------------------
 
 --
@@ -93,8 +169,7 @@ CREATE TABLE `loan_plan` (
 --
 
 INSERT INTO `loan_plan` (`id`, `months`, `interest_percentage`, `penalty_rate`) VALUES
-(4, 2, 10, 1),
-(5, 1, 5, 1);
+(4, 2, 10, 1);
 
 -- --------------------------------------------------------
 
@@ -143,7 +218,42 @@ CREATE TABLE `loan_types` (
 
 INSERT INTO `loan_types` (`id`, `type_name`, `description`) VALUES
 (1, 'Small Business', 'Small Business Loans'),
-(4, 'Big Business', 'Big Business Loans');
+(4, 'Big Business', 'Big Business Loans'),
+(5, 'Medium Business', 'Medium Business Loans');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(5, '0001_01_01_000001_create_cache_table', 1),
+(6, '0001_01_01_000002_create_jobs_table', 1),
+(10, '0001_01_01_000000_create_users_table', 2),
+(14, '2024_11_25_214108_create_borrowers_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -171,29 +281,56 @@ INSERT INTO `payments` (`id`, `loan_id`, `payee`, `amount`, `penalty_amount`, `o
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('XTZSxxI7LwJtZL1MFt9WWgUMS6QlS4nzqhYisGah', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'YTozOntzOjY6Il9mbGFzaCI7YToyOntzOjM6Im5ldyI7YTowOnt9czozOiJvbGQiO2E6MDp7fX1zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czozMjoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL21lbWJlcnNoaXAiO31zOjY6Il90b2tlbiI7czo0MDoiUUFER0U5d3hLMXhMQmRzMGQ1czAwc3FsaWF6SzlEQjRvMzRwZmtZRCI7fQ==', 1732655728);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(30) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `address` text NOT NULL,
-  `contact` text NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(200) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL DEFAULT 'client',
   `email` varchar(255) NOT NULL,
-  `type` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `type` int(11) NOT NULL DEFAULT 0,
+  `password` varchar(255) NOT NULL,
+  `credentials` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `address`, `contact`, `username`, `password`, `role`, `email`, `type`) VALUES
-(17, 'Admin', '', '', 'Admin', '$2y$10$l5MmUO3xb2LDWb.PrF2LfOcZXubHUlsmhixcl1ivHI25yRcasbwsa', 'admin', 'admin@example.com', 0),
-(19, 'Manager', '', '', 'Manager', '$2y$10$v8grwdNUCYGdYgO7ZnM1aeNyWmRhmPYt9JNocGOd2UtWIJvS8eN86', 'manager', 'manager@gmail.com', 0),
-(21, 'Cashier', '', '', 'Cashier', '$2y$10$4XFSkgJE4a3Ezj87fB9VGeMnUIb0eTstjQt0LieAXyNLxUaaclaE2', 'cashier', 'cashier@gmail.com', 0);
+INSERT INTO `users` (`id`, `name`, `address`, `contact`, `username`, `role`, `email`, `email_verified_at`, `type`, `password`, `credentials`, `remember_token`, `created_at`, `updated_at`) VALUES
+(17, 'Admin', '', '', 'Admin', 'admin', 'admin@example.com', NULL, 0, '$2y$10$l5MmUO3xb2LDWb.PrF2LfOcZXubHUlsmhixcl1ivHI25yRcasbwsa', '', NULL, NULL, NULL),
+(19, 'Manager', '', '', 'Manager', 'manager', 'manager@gmail.com', NULL, 0, '$2y$10$v8grwdNUCYGdYgO7ZnM1aeNyWmRhmPYt9JNocGOd2UtWIJvS8eN86', '', NULL, NULL, NULL),
+(21, 'Cashier', '', '', 'Cashier', 'cashier', 'cashier@gmail.com', NULL, 0, '$2y$10$4XFSkgJE4a3Ezj87fB9VGeMnUIb0eTstjQt0LieAXyNLxUaaclaE2', '', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -203,6 +340,38 @@ INSERT INTO `users` (`id`, `name`, `address`, `contact`, `username`, `password`,
 -- Indexes for table `borrowers`
 --
 ALTER TABLE `borrowers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cache`
+--
+ALTER TABLE `cache`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `cache_locks`
+--
+ALTER TABLE `cache_locks`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jobs_queue_index` (`queue`);
+
+--
+-- Indexes for table `job_batches`
+--
+ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -230,16 +399,37 @@ ALTER TABLE `loan_types`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
 -- Indexes for table `payments`
 --
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -249,13 +439,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `borrowers`
 --
 ALTER TABLE `borrowers`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `loan_list`
 --
 ALTER TABLE `loan_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `loan_plan`
@@ -273,7 +475,13 @@ ALTER TABLE `loan_schedules`
 -- AUTO_INCREMENT for table `loan_types`
 --
 ALTER TABLE `loan_types`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `payments`
@@ -285,7 +493,7 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
